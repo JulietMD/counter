e3ec692a7d55f6aa20264a4af86ac4da3b8fe161
Fabien BEHIER <fabien.behier@icloud.com>
2019-02-04 16:21:40 +0000

    layout finished, add homework

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
+const VISIBLE_STYLE = {
+  borderColor: '#000',
+  borderWidth: 1,
+}
+
+const BUTTON_FONT_SIZE = 150
+const INITIAL_COUNT = 10
+
+const ON_ZERO_COLOR = '#FF0000'
+const DEFAULT_COLOR = '#00FF00'
+
+// TODO disable the minus button
+// TODO reset should not be active if the value is alreay the initail one
+// TODO I want from 10 to 0 lower I go, more close to ON_ZERO_COLOR the number should be over 9 if should be DEFAULT_COLOR
+
 export default class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      count: 10
+      count: INITIAL_COUNT
     }
-
-    // fabien is great !!!, no he is AMAZING !!!
   }
 
   HandleReset = () => {
     this.setState({
-      count: 0
+      count: INITIAL_COUNT
     })
   }
 
@@ -34,7 +47,6 @@ export default class App extends React.Component {
           backgroundColor: '#fff',
         }}
       >
-
         <Text
           style={{
             fontSize: 200,
@@ -45,10 +57,19 @@ export default class App extends React.Component {
 
         <View
           style={{
+            width: '70%',
             flexDirection: 'row',
+            justifyContent: 'space-around',
           }}
         >
           <TouchableOpacity
+            style={{
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: BUTTON_FONT_SIZE * 2/3,
+              aspectRatio: 1,
+            }}
             onPress={()=>{
               this.setState({
                 count: count + 1
@@ -57,8 +78,9 @@ export default class App extends React.Component {
           >
             <Text
               style={{
-                fontSize: 100,
-                color: '#00ff00',
+                fontSize: BUTTON_FONT_SIZE,
+                color: '#3FE80C',
+                marginTop: BUTTON_FONT_SIZE * - 1/3,
               }}
             >
               {'+'}
@@ -66,13 +88,26 @@ export default class App extends React.Component {
           </TouchableOpacity>
 
           <TouchableOpacity
+            style={{
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: BUTTON_FONT_SIZE * 2/3,
+              aspectRatio: 1,
+              display: count <= 0 ? 'none' : 'flex',
+            }}
             onPress = {()=>{
               this.setState({
                 count: count - 1
               })
             }}
           >
-            <Text>
+            <Text
+              style={[
+                styles.plusMinusButtonText, 
+                styles.minusButtonText,
+              ]}
+            >
               {'-'}
             </Text>
           </TouchableOpacity>
@@ -80,14 +115,47 @@ export default class App extends React.Component {
         
         <TouchableOpacity
           onPress={this.HandleReset}
+          style={{
+            marginTop: 50,
+          }}
         >
-          <Text>
+          <Text
+            style={{
+              fontSize: 50,
+            }}
+          >
             {'Reset'}
           </Text>
         </TouchableOpacity>
 
       </View>
-
     );
   }
 }
+
+const styles = StyleSheet.create({
+  counter: {
+
+  },
+  plusMinusWrapper: {
+
+  },
+  plusButtonText: {
+  },
+  minusButtonText: {
+    color: '#E82C0C',
+  },
+  plusMinusButtonText: {
+    fontSize: BUTTON_FONT_SIZE,
+    marginTop: BUTTON_FONT_SIZE * - 1/3,
+  },
+  plusMinusButton: {
+
+  },
+  resetButton: {
+
+  },
+  resetButtonText: {
+
+  },
+})
